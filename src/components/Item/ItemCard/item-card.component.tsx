5277de20380b50f1b2f9e22bafdc59ad6dbe8f42
0xleonardo <leonardostavaljladisic@gmail.com>
2023-02-12 19:30:04 +0100

    fix

--- src/components/Item/ItemCard/item-card.component.tsx
+++ src/components/Item/ItemCard/item-card.component.tsx
@@ -4,6 +4,7 @@ import {Item} from "../../../modals/item.modal";
 import {observer} from "mobx-react";
 import {moneyCurrency} from "../../../utils/simple.utils";
 import {Category} from "../../../modals/category.modal";
+import cartStore from "../../../stores/cart.store";
 
 const _ = require('lodash');
 
@@ -18,27 +19,29 @@ export const ItemCard = observer((props: ItemCardProps) => {
     const icon = require(`../../../assets/category/${props.category!.imageName}`);
 
     return (
-        <div className="item_card" onClick={() => navigate(`/item/${props.item.id}`)}>
-            <img src={JSON.parse(props.item.images)[0] ? JSON.parse(props.item.images)[0].url : icon} alt="Monitor"
-                 onClick={() => navigate(`/item/${props.item.id}`)}
-                 style={{backgroundColor: JSON.parse(props.item.images)[0] ? "white" : "gray",
-                 padding: JSON.parse(props.item.images)[0] ? 0 : 10}}/>
-            <div className="item_card_info">
-                <div className="item_card_brand">{props.item.brand}</div>
-                <div className="item_card_series">{props.item.series}</div>
-                <div className="item_card_desc">{props.item.description}</div>
-                {props.item.discount === 0 ?
-                    <div className="item_card_price">
-                        <div className="item_card_price_real">{props.item.price} {moneyCurrency}</div>
-                    </div> :
-                    <div className="item_card_price_discounted">
-                        <div className="item_card_price_real">{props.item.price} {moneyCurrency}</div>
-                        <div
-                            className="item_card_price_discounted">{_.round(props.item.price * (100 - props.item.discount) / 100, 2)} {moneyCurrency}</div>
-                    </div>
-                }
+        <div className="item_card">
+            <div className="item_card_trigger" onClick={() => navigate(`/item/${props.item.id}`)}>
+                <img src={JSON.parse(props.item.images)[0] ? JSON.parse(props.item.images)[0].url : icon} alt="Monitor"
+                     onClick={() => navigate(`/item/${props.item.id}`)}
+                     style={{backgroundColor: JSON.parse(props.item.images)[0] ? "white" : "gray",
+                     padding: JSON.parse(props.item.images)[0] ? 0 : 10}}/>
+                <div className="item_card_info">
+                    <div className="item_card_brand">{props.item.brand}</div>
+                    <div className="item_card_series">{props.item.series}</div>
+                    <div className="item_card_desc">{props.item.description}</div>
+                    {props.item.discount === 0 ?
+                        <div className="item_card_price">
+                            <div className="item_card_price_real">{props.item.price} {moneyCurrency}</div>
+                        </div> :
+                        <div className="item_card_price_discounted">
+                            <div className="item_card_price_real">{props.item.price} {moneyCurrency}</div>
+                            <div
+                                className="item_card_price_discounted">{_.round(props.item.price * (100 - props.item.discount) / 100, 2)} {moneyCurrency}</div>
+                        </div>
+                    }
+                </div>
             </div>
-            <button onClick={() => navigate(`/item/${props.item.id}`)}>Buy now</button>
+            <button onClick={() => cartStore.addItem(props.item)}>Buy now</button>
         </div>
     )
 
